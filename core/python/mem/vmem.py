796e483bcd377135f2f9bbd41bd62822787ce90e
ToadRage <116450979+ToadRage@users.noreply.github.com>
2023-12-05 13:43:33 -0500

    Update and rename pagetable.py to vmem.py

---
+++ core/python/mem/vmem.py
@@ -0,0 +1,11 @@
+#storing memory in 16 slot data racks 
+#                        b[bit#]r[register#]
+#    The only reason I didn't start from 0 and go to 15 is to make this
+#        program more user-friendly, as I plan to make it a framework.
+
+
+
+r0 = ['b1r0','b2r0','b3r0','b4r0','b5r0','b6r0','b7r0','b8r0','b9r0','b10r0','b11r0','b12r0','b13r0','b14r0','b15r0','b16r0']
+r1 = ['b1r1','b2r1','b3r1','b4r1','b5r1','b6r1','b7r1','b8r1','b9r1','b10r1','b11r1','b12r1','b13r1','b14r1','b15r1','b16r1']
+r2 = ['b1r2','b2r2','b3r2','b4r2','b5r2','b6r2','b7r2','b8r2','b9r2','b10r2','b11r2','b12r2','b13r2','b14r2','b15r2','b16r2']
+r3 = ['b1r3','b2r3','b3r3','b4r3','b5r3','b6r3','b7r3','b8r3','b9r3','b10r3','b11r3','b12r3','b13r3','b14r3','b15r3','b16r3']
